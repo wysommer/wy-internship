6d863ac56f30b11471a6890661198b3969bbf32d
wysommer <wyatt3996@gmail.com>
2025-04-13 17:03:21 -0600

    made explore page dynamic, added routing

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -99,7 +99,7 @@ const ExploreItems = () => {
             <div className="nft__item">
               <div className="author_list_pp">
                 <Link
-                  to="/author"
+                  to={`/author/${item.authorId}`}
                   data-bs-toggle="tooltip"
                   data-bs-placement="top"
                 >
@@ -131,7 +131,7 @@ const ExploreItems = () => {
                     </div>
                   </div>
                 </div>
-                <Link to="/item-details">
+                <Link to={`/item-details/${item.nftId}`}>
                   <img
                     src={item.nftImage}
                     className="lazy nft__item_preview"
@@ -140,7 +140,7 @@ const ExploreItems = () => {
                 </Link>
               </div>
               <div className="nft__item_info">
-                <Link to="/item-details">
+                <Link to={`/item-details/${item.nftId}`}>
                   <h4>{item.title}</h4>
                 </Link>
                 <div className="nft__item_price">{item.price} ETH</div>
